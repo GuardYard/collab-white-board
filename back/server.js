8d206162d59ee580858bd59278c413f396c3e822
Fathi <fathi.kadir.a@gmail.com>
2021-12-02 01:29:33 +0000

    add functionning user interaction back

--- back/server.js
+++ back/server.js
@@ -1,8 +1,22 @@
-let app = require('express')();
+let express = require('express');
+let app = express();
 const cors = require('cors');
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
+const mongoose = require('mongoose');
+
 app.use(cors());
+app.use(express.json());
+let serv_port = 5000 ;
+
+mongoose.connect("mongodb+srv://group:fLJzHxm7XmRTSf2y@cluster0.fvehm.mongodb.net/collab_whiteboard?retryWrites=true&w=majority")
+    .catch(err => {
+        console.log("Authentification to mongoose failed")
+        console.log(err.message)
+    });
+
+mongoose.connection.once('open', () => { console.log("MongoDB database connection established successfully") });
+
 
 io.on('connection', (socket) => {
     console.log("New user online");
@@ -11,7 +25,12 @@ io.on('connection', (socket) => {
     })
 })
 
-let serv_port = process.env.LOCAL_PORT || process.env.PORT || 5000;
+const userRoutes = require('./routes/users')
+const authRoutes = require('./routes/authentification')
+
+app.use('/', authRoutes);
+app.use('/users', userRoutes);
+
 http.listen(serv_port, () => {
     console.log("Port: " + serv_port);
 })
