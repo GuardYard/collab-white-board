ac490c3755ac8a17d2e07ce09a726f9654431f8e
Fathi <fathi.kadir.a@gmail.com>
2021-12-02 01:31:54 +0000

    small import impotimization

--- back/routes/users.js
+++ back/routes/users.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const passwordServices = require('../services/passwordServices')
 
 const User = require('../models/user.model');
 
